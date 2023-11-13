efdae314171345f0eb836bf44d739cb6f9cc02aa
WesleyR10 <wesleyribas2015@gmail.com>
2023-11-13 15:49:42 -0300

    Create token authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express")
 const usersRoutes = require("./users.routes")
 const moviesRoutes = require("./movies.routes")
 const tagsRoutes = require("./tags.routes")
+const sessionsRouter = require("./sessions.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRoutes)
+routes.use("/sessions", sessionsRouter);
 routes.use("/movies", moviesRoutes)
 routes.use("/tags", tagsRoutes)
 
