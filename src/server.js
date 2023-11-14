2dc1c12dc4947df35bbdaa385853ad801f1f4423
WesleyR10 <wesleyribas2015@gmail.com>
2023-11-14 20:13:49 -0300

    Implementation Pm2 - dotenv

--- src/server.js
+++ src/server.js
@@ -28,5 +28,5 @@ app.use((error, req, res, next) => {
   })
 })
 
-const PORT = 3000
+const PORT = process.env.SERVER_PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
